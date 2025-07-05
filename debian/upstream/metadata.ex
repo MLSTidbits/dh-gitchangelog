b4298d3cd7414287b91274f21ae072f325dc5f17
Michael Schaecher <MichaelLeeSchaecher@gmail.com>
2025-07-05 08:02:38 -0600

    Add Debian packaging files and configuration for a true `deb` package build process

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/dpkg-changelog/issues
+# Bug-Submit: https://github.com/<user>/dpkg-changelog/issues/new
+# Changelog: https://github.com/<user>/dpkg-changelog/blob/master/CHANGES
+# Documentation: https://github.com/<user>/dpkg-changelog/wiki
+# Repository-Browse: https://github.com/<user>/dpkg-changelog
+# Repository: https://github.com/<user>/dpkg-changelog.git
